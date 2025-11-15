43f82f7e39303b29870717f59590ffe2f35623fb
Bruce Ambundo <bruceambundo@gmail.com>
2025-11-15 12:34:03 +0300

    Services section ready

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Home.css'
 import Hero from './home-components/hero/Hero'
 import About from './home-components/about/About'
+import Services from './home-components/services/Services'
 
 export default function Home() {
   return (
     <div>
         <Hero />
         <About />
+        <Services />
     </div>
   )
 }
