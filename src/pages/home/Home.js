f243a4e395b323b2d79b156c0661ab82a8854dc0
Bruce Ambundo <bruceambundo@gmail.com>
2025-11-18 12:45:15 +0300

    Pricing section complete

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,7 @@ import Hero from './home-components/hero/Hero'
 import About from './home-components/about/About'
 import Services from './home-components/services/Services'
 import WhyUs from './home-components/whyus/WhyUs'
+import Pricing from './home-components/pricing/Pricing'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <About />
         <Services />
         <WhyUs />
+        <Pricing />
     </div>
   )
 }
