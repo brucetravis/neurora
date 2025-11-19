b044e21da6d627a144db83f7a840bcf0a1e592ea
Bruce Ambundo <bruceambundo@gmail.com>
2025-11-19 09:04:43 +0300

    about page back to original state

--- src/pages/home/home-components/about/About.js
+++ src/pages/home/home-components/about/About.js
@@ -1,65 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './About.css'
-// import HologramSphere from '../../../../components/hologramsphere/HologramSphere'
-import RobotGif from '../../../../videos/robot-unscreen.gif'
-// import { Canvas, useFrame } from '@react-three/fiber'
-// import { useTexture } from '@react-three/drei'
-// import * as THREE from 'three';
-
-// function FloatingGif() {
-  
-//   const meshRef = React.useRef()
-//   const texture = useTexture(RobotGif)
-  
-
-//   useFrame(({ clock }) => {
-//     const t = clock.getElapsedTime()
-
-//     // Floating motion
-//     meshRef.current.position.y = Math.sin(t * 1.5) * 0.15
-//     // meshRef.current.position.y = Math.sin(t * 0.3) * 0.3
-//     meshRef.current.rotation.y = Math.sin(t * 0.5) * 0.1;
-  
-//     // Pulsing glow for the hologram
-//     meshRef.current.children[0].material.opacity = 0.1 + Math.sin(t * 3) * 0.05;
-//   })
-
-
-//   return (
-//     // <mesh ref={meshRef}>
-//     //   <planeGeometry args={[9, 10]} />
-//     //   <meshBasicMaterial map={texture} transparent />
-
-
-//     // </mesh>
-//     <group ref={meshRef}>
-//       {/* Hologram glow plane */}
-//       <mesh scale={[1.1, 1.1, 1]}>
-//         <planeGeometry args={[9, 10]} />
-//         <meshBasicMaterial
-//           color="#00ffff"
-//           transparent
-//           opacity={0.15}
-//           toneMapped={false}
-//           blending={THREE.AdditiveBlending}
-//         />
-//       </mesh>
-
-//       {/* Robot GIF plane */}
-//       <mesh>
-//         <planeGeometry args={[9, 10]} />
-//         <meshBasicMaterial map={texture} transparent />
-//       </mesh>
-//     </group>
-//   )
-// }
 import { useSpring, animated, useInView } from '@react-spring/web'
-// import HologramSphere from '../../../../components/hologramsphere/HologramSphere'
+import HologramSphere from '../../../../components/hologramsphere/HologramSphere'
 
 export default function About() {
 
   // states to control the section view
-  const [ ref, inView ] = useInView({ triggerOnce: true, threshold: 0.2 })
+  const [ ref, inView ] = useInView(({ triggerOnce: true, threshold: 0.2 }))
 
   // // stagger: title then paragraph
   const titleSpring = useSpring({
@@ -77,13 +24,6 @@ export default function About() {
   });
 
 
-  const imageSpring = useSpring({
-    opacity: inView ? 1 : 0,
-    transform: inView ? 'translateX(0px)' : 'translateX(-30px)',
-    // transform: inView ? 'transformX(0px)' : 'transformX(-30px)',
-    config: { tension: 200, friction: 22 },
-    delay: 250
-  })
 
   return (
     <section className="about-hero" aria-labelledby='about-title' ref={ref}>
@@ -95,33 +35,8 @@ export default function About() {
       <div
         className='about-inner container'
       >
-        <div 
-          className='about-left col-md-6 col-lg-6'
-          // style={{ 
-          //   backgroundImage: `url(${RobotGif})`,
-          //   backgroundSize: "cover", 
-          //   backgroundPosition: "center" 
-          // }}
-        >
-          {/* <HologramSphere /> */}
-
-          <animated.img 
-            src={RobotGif} 
-            alt="Robot animation" 
-            style={{
-              ...imageSpring,
-              width: "100%", 
-              height: "100%", 
-              objectFit: "cover" 
-            }} 
-          />
-
-          {/* <Canvas style={{ width: "100%", height: "400px" }}>
-            <ambientLight intensity={1} />
-            <FloatingGif />
-          </Canvas> */}
-
-
+        <div className='about-left col-md-6 col-lg-6'>
+          <HologramSphere />
         </div>
 
         <div
