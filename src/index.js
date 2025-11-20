4b742b7a76039169364ec617b15e8e310107bd76
Bruce Ambundo <bruceambundo@gmail.com>
2025-11-20 11:06:59 +0300

    home page upgraded

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/header/Header';
+import ActiveProvider from './contexts/active/ActiveContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Header />
-      <App />
-    </BrowserRouter>
+    <ActiveProvider>
+      <BrowserRouter>
+      <Header />
+        <App />
+      </BrowserRouter>
+    </ActiveProvider>
   </React.StrictMode>
 );
