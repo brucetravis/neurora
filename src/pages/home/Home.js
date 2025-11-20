9820d0fe83601a4c75478403a1f70af8daf85d2a
Bruce Ambundo <bruceambundo@gmail.com>
2025-11-20 09:46:35 +0300

    Home page scrolls to the top on mount

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Home.css'
 import Hero from './home-components/hero/Hero'
 import About from './home-components/about/About'
@@ -8,6 +8,17 @@ import Pricing from './home-components/pricing/Pricing'
 import Contact from './home-components/contact/Contact'
 
 export default function Home() {
+
+  useEffect(() => {
+    
+    // deay to ensure browser layout is ready before scrolling
+    const timer = setTimeout(() => {
+      window.scrollTo({ top: 0, left: 0, smooth: 0 })
+    }, 0)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div>
         <Hero />
