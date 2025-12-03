371eb700fd0a0fce494c675750a4263d957f0c19
Bruce Ambundo <bruceambundo@gmail.com>
2025-12-03 09:21:27 +0300

    Footer adde to index.js file

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
+import Footer from './components/footer/Footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Header />
+      <Header />
       <App />
-    <Footer />
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
