bfbd49670fe777367dc137c95d5ea4558d14e55c
Bruce Ambundo <bruceambundo@gmail.com>
2025-12-03 08:47:39 +0300

    Pricing modals working. Payment integration remaining

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
-// import StandardPricingModal from './components/modals/standard/StandardPricingModal';
-// import { usePricing } from './contexts/PricingProvider';
-// import { aiPlans, swPlans } from './data/Pricing';
+import StandardPricingModal from './components/modals/standard/StandardPricingModal';
+import { usePricing } from './contexts/PricingProvider';
+import { aiPlans, swPlans } from './data/Pricing';
+import CustomQuoteModal from './components/modals/custom/CustomQuoteModal';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
-  // const { openStandardModal } = usePricing();
+  const { openStandardModal, openQuotationModal } = usePricing();
 
   return (
     <>
@@ -17,7 +20,22 @@ function App() {
         </Routes>
       </div>
 
-      {/* {openStandardModal && <StandardPricingModal aiPlans={aiPlans} swPlans={swPlans} />} */}
+      {openStandardModal && <StandardPricingModal aiPlans={aiPlans} swPlans={swPlans} />}
+      {openQuotationModal && <CustomQuoteModal />}
+
+      <ToastContainer 
+        position="bottom-left"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme=''
+      />
+    
     </>
   );
 }
