04f64220e1891d44bffe5eed684013c2583b33e7
Bruce Ambundo <bruceambundo@gmail.com>
2026-03-18 18:31:29 +0300

    Hero section created and responsive

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import ScrollProvider from './contexts/scroll/ScrollContext';
+import ActiveProvider from './contexts/active/ActiveContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ScrollProvider>
+        <ActiveProvider>
+          <App />
+        </ActiveProvider>
+      </ScrollProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
